6927b9598537b4a55ad82100b5109fed23417fc5
SergikArust <sergey-arustamyan@mail.ru>
2025-01-20 16:06:17 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,12 @@
+
+def divide(a, b):
+    if b != (0):
+        d = a/b
+        return d
+    else:
+        return 'Ошибка'
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(a, b):
+    if b !=0:
+        d = a/b
+        return d
+    else:
+        return inf
